0854d89cb871f47f2539507fe73ff717963ff3c0
gatorolo <rodrigo.daremberg@gmail.com>
2025-05-04 10:29:35 -0300

    mejorando la interfaz

--- Pruebas.java
+++
@@ -1,7 +0,0 @@
-public class Pruebas {
-
-    public ResponseEntity<List<Producto>> getProduct() {
-        List<Producto> productos = todoRepository.getProduct();
-        return new ResponseEntity<>(productos , HttpStatus.ok);
-    }
-}
